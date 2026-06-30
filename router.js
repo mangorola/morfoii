1a7b909f07c998fb1422a33a29372c9561d164c1
inaki <inakimatias@gmail.com>
2026-06-30 01:11:37 -0300

    ajustes de diseño

--- router.js
+++ router.js
@@ -12,7 +12,7 @@
     '01': 'cap-01-focus.html',
     '02': 'cap-02-path.html',
     '03': 'cap-03-adaptability.html',
-    '04': 'cap-04.html',
+    '04': 'cap-04-coherence.html',
     '05': 'cap-05.html',
     '06': 'cap-06.html',
     '07': 'cap-07.html',
